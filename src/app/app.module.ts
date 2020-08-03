632ca2116c812b27c80ab348008685bb622dba46
albertogarza <betogf@gmail.com>
2020-08-03 17:22:19 -0600

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatToolbarModule
+    FormsModule
     ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
